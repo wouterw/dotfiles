a2de672d0ae4d3397c8db91a1465acfd74f1f456
Wouter Willaert <wouter.willaert@gmail.com>
2018-02-05 21:44:37 +0100

    Load `asdf` and `vscode`

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,12 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
+# load asdf
+if [ -d "$HOME/.asdf" ]; then
+  . $HOME/.asdf/asdf.sh
+  . $HOME/.asdf/completions/asdf.bash
+fi
+
 # load rbenv if available
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
@@ -16,7 +22,7 @@ export PATH="$HOME/.yarn/bin:$PATH"
 # load cargo
 [[ -f ~/.cargo/env ]] && source ~/.cargo/env
 
-# Add Visual Studio Code (code)
-export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+# load vscode
+export PATH="$PATH:/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin"
 
 export -U PATH
