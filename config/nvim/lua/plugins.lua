57623c3e2bd0e6f83adebf065abe509853600cd3
Wouter Willaert <wouter.willaert@gmail.com>
2021-12-23 10:29:36 +0100

    Rework Packer configuration

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,16 +1,37 @@
-local execute = vim.api.nvim_command
 local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local execute = vim.api.nvim_command
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
   execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  execute "packadd packer.nvim"
+  execute("packadd packer.nvim")
 end
 
-require('packer').startup(function()
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+packer.startup(function()
   use 'wbthomason/packer.nvim'
 
   use {
@@ -64,11 +85,6 @@ require('packer').startup(function()
     config = function()
       require("config.telescope")
     end
-  }
-
-  use {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    run = 'make'
   }
 
   use {
