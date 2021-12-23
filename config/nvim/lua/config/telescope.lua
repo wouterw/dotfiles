230936fc0992304caef01679f32ec0aeb55d690c
Wouter Willaert <wouter.willaert@gmail.com>
2021-12-23 10:29:53 +0100

    Remove telescope fzf extension

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -6,17 +6,7 @@ require("telescope").setup {
     selection_caret = " ",
     mappings = {i = {["<esc>"] = actions.close, ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist}}
   },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
-  }
 }
-
-require('telescope').load_extension('fzf')
 
 local M = {}
 
