59d4347608d4892b24edd551af4e608f1fe5a489
Wouter Willaert <wouter.willaert@gmail.com>
2022-06-19 16:48:13 +0200

    switch between light and dark theme

--- config/nvim/lua/themes/github.lua
+++ config/nvim/lua/themes/github.lua
@@ -1,5 +1,10 @@
-require("github-theme").setup({
-	theme_style = "dark_default",
-	dark_sidebar = false,
-	transparent = true,
+local function is_dark_mode()
+  local result = vim.fn.systemlist('defaults read -g AppleInterfaceStyle')
+  return result[1] == 'Dark'
+end
+
+require('github-theme').setup({
+  theme_style = is_dark_mode() and 'dark_default' or 'light_default',
+  dark_sidebar = false,
+  transparent = true,
 })
