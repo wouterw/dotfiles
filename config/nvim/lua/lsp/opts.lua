ba5e82070fe15b2c77f4d23cc74c3d1825a06649
Wouter Willaert <wouter.willaert@gmail.com>
2022-11-01 10:55:57 +0100

    Update lsp config

--- config/nvim/lua/lsp/opts.lua
+++ config/nvim/lua/lsp/opts.lua
@@ -43,7 +43,7 @@ local opts = {
 
     print('LSP Attached.')
   end,
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 }
 
 return opts
