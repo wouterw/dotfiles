e0c40bc46165631e2394150b864139276da4da55
Wouter Willaert <wouter.willaert@gmail.com>
2022-11-01 10:49:34 +0100

    Update nvim-tree config

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -1,37 +1,40 @@
-require("nvim-tree").setup({
-	view = {
-		hide_root_folder = true,
-		signcolumn = "no",
-	},
-	renderer = {
-		highlight_git = true,
-		indent_markers = { enable = true },
-		icons = {
-			webdev_colors = false,
-			show = {
-				file = false,
-				folder = false,
-				folder_arrow = true,
-				git = false,
-			},
-			glyphs = {
-				folder = {
-					arrow_closed = "●",
-					arrow_open = "○",
-					default = "●",
-					empty = "◌",
-					empty_open = "○",
-					open = "○",
-				},
-			},
-		},
-		special_files = {},
-	},
-	update_focused_file = { enable = true },
-	diagnostics = { enable = true },
+require('nvim-tree').setup({
+  view = {
+    hide_root_folder = true,
+    signcolumn = 'no',
+  },
+  renderer = {
+    highlight_git = true,
+    indent_markers = { enable = true },
+    icons = {
+      webdev_colors = false,
+      show = {
+        file = false,
+        folder = false,
+        folder_arrow = true,
+        git = false,
+      },
+      glyphs = {
+        folder = {
+          arrow_closed = '●',
+          arrow_open = '○',
+          default = '●',
+          empty = '◌',
+          empty_open = '○',
+          open = '○',
+        },
+      },
+    },
+    special_files = {},
+  },
+  update_focused_file = { enable = true },
+  diagnostics = { enable = true },
+  filters = {
+    custom = { '^\\.git' },
+  },
 })
 
 -- Mappings
-vim.keymap.set("n", "<C-n>", function()
-	require("nvim-tree").toggle()
+vim.keymap.set('n', '<C-n>', function()
+  require('nvim-tree').toggle()
 end, { noremap = true, silent = true })
