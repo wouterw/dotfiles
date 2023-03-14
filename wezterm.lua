93b007a756802cb3f0d0ee98520cbe5911d75383
Wouter Willaert <wouter.willaert@gmail.com>
2023-03-14 10:13:07 +0100

    Add wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("Berkeley Mono"),
+	font_size = 12.0,
+	color_scheme = "Jellybeans",
+	window_background_opacity = 0.95,
+	window_decorations = "RESIZE",
+	window_padding = { left = "2cell", right = "2cell", top = "0.5cell", bottom = "0.5cell" },
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+}
