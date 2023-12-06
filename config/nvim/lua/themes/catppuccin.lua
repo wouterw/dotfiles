31ab28dde5f4822f4ffbda7163f35d2ade6bda01
Wouter Willaert <wouter.willaert@gmail.com>
2023-12-06 08:37:55 +0100

    Update colors

--- config/nvim/lua/themes/catppuccin.lua
+++ config/nvim/lua/themes/catppuccin.lua
@@ -2,6 +2,16 @@
 
 -- local flavour = (is_dark_mode() and 'mocha' or 'latte')
 
-require('catppuccin').setup({ flavour = 'mocha', transparent_background = true })
+require('catppuccin').setup({
+  flavour = 'mocha',
+  transparent_background = false,
+  color_overrides = {
+    mocha = {
+      base = '#000000',
+      mantle = '#000000',
+      crust = '#000000',
+    },
+  },
+})
 
 vim.cmd.colorscheme('catppuccin')
