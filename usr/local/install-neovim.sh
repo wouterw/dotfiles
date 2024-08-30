69b1e45d15dc97cde687623c8088cf594a4eeb21
Wouter Willaert <wouter.willaert@gmail.com>
2024-08-30 15:55:11 +0200

    wip

--- usr/local/install-neovim.sh
+++ usr/local/install-neovim.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -d "./nvim-macos-arm64" ]; then
-  mv ./nvim-macos-arm64 ./nvim-macos-arm64-old
+if [ -d "/usr/local/bin/nvim-macos-arm64" ]; then
+  mv /usr/local/bin/nvim-macos-arm64 /usr/local/bin/nvim-macos-arm64-old
 fi
 
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
-xattr -c ./nvim-macos-arm64.tar.gz
-tar xzvf nvim-macos-arm64.tar.gz
-sudo ln -s /usr/local/bin/nvim-macos-arm64/bin/nvim /usr/local/bin/nvim
+curl -LO -o /usr/local/bin/nvim-macos-arm64.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
+xattr -c /usr/local/bin/nvim-macos-arm64.tar.gz
+tar xzvf /usr/local/bin/nvim-macos-arm64.tar.gz
 
-if [ -d "./nvim-macos-arm64" ]; then
-  rm -rf ./nvim-macos-arm64-old
-fi
+ln -s /usr/local/bin/nvim-macos-arm64/bin/nvim /usr/local/bin/nvim
+
+if [ -d "./nvim-macos-arm64" ]; then rm -rf ./nvim-macos-arm64-old; fi
+if [ -d "./nvim-macos-arm64.tar.gz" ]; then rm ./nvim-macos-arm64.tar.gz; fi
 
 echo "Neovim installation complete."
