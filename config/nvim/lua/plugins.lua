50dd1e2c78271773242c8bbc98591f9e0620e452
Wouter Willaert <wouter.willaert@gmail.com>
2024-11-04 07:49:01 +0100

    wip [ci skip]

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -16,6 +16,28 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 
   {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('themes.catppuccin')
+    end,
+  },
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    end,
+  },
+
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
